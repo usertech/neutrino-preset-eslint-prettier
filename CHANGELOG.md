284eac9936fdf40d9f7cd7dbbf7652f7b8ef9354
Jiri Brabec <brabeji@gmail.com>
2018-09-10 14:30:50 +0200

    Do not lint when running devserver, disable react/prop-types rule

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.0.3
+
+- Do not lint when running devserver, disable react/prop-types rule
--- index.js
+++ index.js
@@ -13,34 +13,38 @@ const prettierrc = () => {
 };
 
 module.exports = (neutrino, { eslint: eslintOptions = {} } = {}) => {
-	neutrino.use(eslint, {
-		test: neutrino.regexFromExtensions(),
-		include: [neutrino.options.source, neutrino.options.tests],
-		exclude: [],
-		eslint: {
-			failOnError: process.env.NODE_ENV !== 'development',
-			cwd: neutrino.options.root,
-			useEslintrc: false,
-			baseConfig: {
-				extends: [
-					'eslint:recommended',
-					'plugin:react/recommended',
-					'plugin:flowtype/recommended',
-					'prettier',
-				],
+	// do not lint when running devserver
+	if (neutrino.options.command !== 'start') {
+		neutrino.use(eslint, {
+			test: neutrino.regexFromExtensions(),
+			include: [neutrino.options.source, neutrino.options.tests],
+			exclude: [],
+			eslint: {
+				failOnError: process.env.NODE_ENV !== 'development',
+				cwd: neutrino.options.root,
+				useEslintrc: false,
+				baseConfig: {
+					extends: [
+						'eslint:recommended',
+						'plugin:react/recommended',
+						'plugin:flowtype/recommended',
+						'prettier',
+					],
+				},
+				plugins: ['prettier', 'flowtype'],
+				// envs: ['es6'],
+				parserOptions: {
+					ecmaVersion: 2018,
+					sourceType: 'module',
+				},
+				globals: ['process', 'window', 'document', 'navigator', 'console', 'fetch'],
+				rules: {
+					'prettier/prettier': ['error', {}],
+					'react/prop-types': 0,
+				},
 			},
-			plugins: ['prettier', 'flowtype'],
-			// envs: ['es6'],
-			parserOptions: {
-				ecmaVersion: 2018,
-				sourceType: 'module',
-			},
-			globals: ['process', 'window', 'document', 'navigator', 'console', 'fetch'],
-			rules: {
-				'prettier/prettier': ['error', {}],
-			},
-		},
-	});
+		});
+	}
 	neutrino.register(
 		'prettierrc',
 		prettierrc,
